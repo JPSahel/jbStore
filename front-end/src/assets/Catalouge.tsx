99dc30ff8caedffa9d70fb7a38647b5d2828e0ed
JPSahel <jpsahelsk@gmail.com>
2025-01-09 21:11:02 -0500

    Published Backend

--- front-end/src/assets/Catalouge.tsx
+++ front-end/src/assets/Catalouge.tsx
@@ -25,7 +25,9 @@ const Catalogue: React.FC<CatalogueProps> = ({ activeFlavors, addToCart }) => {
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/products");
+        const response = await axios.get(
+          "https://privjbstore.onrender.com/api/products"
+        );
         setProducts(response.data);
         setLoading(false);
       } catch (error) {
